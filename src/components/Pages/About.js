c7f0830fb21247f9ba499dc39dc66640fea35805
Kimberly Heidorn <heidorkn@gmail.com>
2019-09-04 16:07:28 -0700

    Building About Me page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div>
-
+      <div className="About-page">
+        <img id="Pool-photo"
+          src="https://i.imgur.com/rvzY26z.png"
+          alt="Kim Playing Pool"
+        />
+        <div className="About-text-field">
+          <p id="Title-about">Hello!</p>
+          <p id="Text-body-about"> </p>
+        </div>
       </div>
     )
   }
